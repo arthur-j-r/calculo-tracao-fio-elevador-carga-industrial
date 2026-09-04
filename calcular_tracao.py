f1ae9ca1d32963059f0bf923c22ed8c87b2b66cb
Arthur José Rocha <arthurjoserocha802@gmail.com>
2026-09-04 16:17:58 -0300

    calcula limite de ruptura e a massa maxima de acordo com a aceleração

--- calcular_tracao.py
+++ calcular_tracao.py
@@ -2,13 +2,36 @@ from scipy import constants
 import math
 gravidade = constants.g
 
-
+# RESISTENCIA DO CABO EM MPA, SECAO DO CABO EM MM², MASSA EM KG, ACELERACAO DO MOTOR EM M/S².
 class CalcularTracao:
-    def __init__(self, massa, aceleracao_do_motor, secao_cabo = None, fator_de_seguranca = 10, resistencia_do_cabo):
+    def __init__(self, massa=None, aceleracao_do_motor=None,resistencia_do_cabo=None, secao_cabo=None, fator_de_seguranca = 10):
         self.massa = massa
         self.aceleracao_do_motor = aceleracao_do_motor
-        self.fator_de_seguranca = fator_de_seguranca
-        self.resistencia_do_cabo = resistencia_do_cabo
+        self.fator_de_seguranca = fator_de_seguranca #PADRAO = 10. CONSULTE MANUAIS PARA O SABER QUAL O MAIS INDICADO PARA SEU PROCESSO
+        if resistencia_do_cabo:
+            self.resistencia_do_cabo = resistencia_do_cabo
+        else:
+            while True:
+                tipo_cabo = input("Digite o tipo do cabo (Aço de alta resistência, Aço Inox (AISI316), Aço Comum, Cobre Revestido, Nylon/Poliamida): ").lower().split()
+
+                if any(p in ("alta", "resistência", "resistencia") for p in tipo_cabo):
+                    self.resistencia_do_cabo = 1770
+                    break
+                elif any(p == "inox" for p in tipo_cabo):
+                    self.resistencia_do_cabo = 1570
+                    break
+                elif any(p == "comum" for p in tipo_cabo):
+                    self.resistencia_do_cabo = 1370
+                    break
+                elif any(p == "cobre" for p in tipo_cabo):
+                    self.resistencia_do_cabo = 200
+                    break
+                elif any(p in ("nylon", "poliamida") for p in tipo_cabo):
+                    self.resistencia_do_cabo = 90
+                    break
+                else:
+                    print("Tipo de cabo não conhecido / não cadastrado. Tente novamente.")
+
         if secao_cabo:
             self.secao_cabo = secao_cabo
         else:
@@ -16,7 +39,26 @@ class CalcularTracao:
             self.secao_cabo = (math.pi * diametro**2) / 4
             print(f"A seção do cabo é: {self.secao_cabo:.2f} mm²")
 
-    def calcular_massa_max(self):
-        
+    def calcular_limite(self):
+        limite = self.resistencia_do_cabo / self.fator_de_seguranca
+        return limite
+    
+    def calcular_massa_max(self,aceleracao=None):
+        limite = self.calcular_limite()
+        if self.aceleracao_do_motor:
+            self.aceleracao_do_motor = aceleracao
+            acel = self.aceleracao_do_motor
+        else:
+            while True:
+                try:
+                    acel = float(input("Insira a aceleração estimada: (em m/s²): "))
+                    break
+                except ValueError:
+                    print("tente novamente. Não use ','. Utilize '.' no lugar. Não utilize strings")
 
-calcular_tracao = CalcularTracao(10, 2)
+        massa_max =  (limite * self.secao_cabo) / (gravidade + acel)
+        print(gravidade)
+        return massa_max
+
+calcular_tracao = CalcularTracao()
+print(calcular_tracao.calcular_massa_max())
